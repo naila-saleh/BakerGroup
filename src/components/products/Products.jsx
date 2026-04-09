94ddaa048dbcd8d331be49ee1d02fcb2e0fc3a64
naila-saleh <nailasaleh2004@gmail.com>
2026-04-09 19:19:36 +0300

    improve design for products & categories pages

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -5,35 +5,45 @@ import Loader from "../../ui/loader/Loader.jsx";
 import Typography from "@mui/material/Typography";
 import {Card, CardContent, CardMedia, Container, Grid} from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {useTranslation} from "react-i18next";
+import bg from "../../assets/images/hero/bg-hero.png";
+import Features from "../features/Features.jsx";
 
 export default function Products() {
     const {data, isLoading, isError, error} = useProducts();
     const {t} = useTranslation();
+    const { pathname } = useLocation();
+    const isHome = pathname === '/';
     if(isLoading) return <Loader />
     if(isError) return <Box>{error.message}</Box>
     return (
-        <Container maxWidth={'xl'} className={'products'} component={'section'} sx={{py: {md: 10, xs: 5}, px: {lg: 10, md: 5, sm: 5, xs: 2}}}>
-            <Typography component={'h2'} sx={{fontSize: {md: '40px', sm: '35px', xs: '32px'}, fontWeight: '500', pb: {md: 3,sm: 2, xs: 1}}}>{t('Trending Products')}</Typography>
-            <Grid container spacing={{lg: 3, xs: 2}}>
-                {data.response.data.map(product=>
-                    <Grid key={product.id} item size={{lg: 3, md: 4, sm: 6, xs: 12}}>
-                        <Link to={`/products/${product.id}`} style={{textDecoration: 'none'}}>
-                            <Card sx={{boxShadow: 3, borderRadius: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 1, cursor: 'pointer'}}>
-                                <CardMedia component={'img'} image={product.image} sx={{width: {md: '100%', sm: '90%', xs: '70%'}, display: 'flex', alignSelf: 'center'}}></CardMedia>
-                                <CardContent sx={{backgroundColor: '#2D5356', color: 'info.light', borderRadius: 4, display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 2, ":last-child": {paddingBottom: 2}}}>
-                                    <Box>
-                                        <Typography component={'h3'}>{product.name}</Typography>
-                                        <Typography component={'span'} variant={'body1'}>${product.price}</Typography>
-                                    </Box>
-                                    <ShoppingCartIcon sx={{color: 'secondary.main', backgroundColor: 'info.light', fontSize: '45px', padding: '10px', borderRadius: '50%', cursor: 'pointer'}} />
-                                </CardContent>
-                            </Card>
-                        </Link>
-                    </Grid>
-                )}
-            </Grid>
-        </Container>
+        <>
+            {!isHome?<Box sx={{backgroundImage: `url(${bg})`, backgroundSize: 'cover', height: '100%', paddingY: {lg: 15,md: 12, sm: 10, xs: 8}}}>
+                <Typography component={'h1'} sx={{color: 'info.light',fontSize: {lg: '45px', md: '40px', sm: '35px', xs: '30px'}, textAlign: 'center'}}>{t('Products')}</Typography>
+            </Box>: null}
+            <Container maxWidth={'xl'} className={'products'} component={'section'} sx={{py: {md: 10, xs: 5}, px: {lg: 10, md: 5, sm: 5, xs: 2}}}>
+                {isHome?<Typography component={'h2'} sx={{fontSize: {md: '40px', sm: '35px', xs: '32px'}, fontWeight: '500', pb: {md: 3,sm: 2, xs: 1}}}>{t('Trending Products')}</Typography>:null}
+                <Grid container spacing={{lg: 3, xs: 2}}>
+                    {data.response.data.map(product=>
+                        <Grid key={product.id} item size={{lg: 3, md: 4, sm: 6, xs: 12}}>
+                            <Link to={`/products/${product.id}`} style={{textDecoration: 'none'}}>
+                                <Card sx={{boxShadow: 3, borderRadius: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 1, cursor: 'pointer'}}>
+                                    <CardMedia component={'img'} image={product.image} sx={{width: {md: '100%', sm: '90%', xs: '70%'}, display: 'flex', alignSelf: 'center'}}></CardMedia>
+                                    <CardContent sx={{backgroundColor: '#2D5356', color: 'info.light', borderRadius: 4, display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 2, ":last-child": {paddingBottom: 2}}}>
+                                        <Box>
+                                            <Typography component={'h3'}>{product.name}</Typography>
+                                            <Typography component={'span'} variant={'body1'}>${product.price}</Typography>
+                                        </Box>
+                                        <ShoppingCartIcon sx={{color: 'secondary.main', backgroundColor: 'info.light', fontSize: '45px', padding: '10px', borderRadius: '50%', cursor: 'pointer'}} />
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        </Grid>
+                    )}
+                </Grid>
+            </Container>
+            {!isHome?<Features />:null}
+        </>
     )
 }
--- src/pages/categories/CategoriesPage.jsx
+++ src/pages/categories/CategoriesPage.jsx
@@ -4,18 +4,30 @@ import Loader from "../../ui/loader/Loader.jsx";
 import Box from "@mui/material/Box";
 import {Grid} from "@mui/material";
 import Category from "../../ui/category/Category.jsx";
+import {useLocation} from "react-router-dom";
+import {useTranslation} from "react-i18next";
+import Typography from "@mui/material/Typography";
+import bg from "../../assets/images/hero/bg-hero.png";
 
 export default function CategoriesPage() {
     const {data, isLoading, isError, error} = useCategories();
+    const { pathname } = useLocation();
+    const isHome = pathname === '/';
+    const {t} = useTranslation();
     if(isLoading) return <Loader />
     if(isError) return <Box>{error.message}</Box>
     return (
-        <Grid container component={'section'} py={5} spacing={3} sx={{display: 'flex', justifyContent: 'center'}}>
-            {data.response.data.map((category) => (
-                <Grid key={category.id} size={{lg: 2.3, md: 3, sm: 4, xs: 6}} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 3, cursor: 'pointer'}}>
-                    <Category {...category} />
-                </Grid>
-            ))}
-        </Grid>
+        <>
+            {!isHome?<Box sx={{backgroundImage: `url(${bg})`, backgroundSize: 'cover', height: '100%', paddingY: {lg: 15,md: 12, sm: 10, xs: 8}}}>
+                <Typography component={'h1'} sx={{color: 'info.light',fontSize: {lg: '45px', md: '40px', sm: '35px', xs: '30px'}, textAlign: 'center'}}>{t('Categories')}</Typography>
+            </Box>:null}
+            <Grid container component={'section'} py={5} spacing={3} sx={{display: 'flex', justifyContent: 'center'}}>
+                {data.response.data.map((category) => (
+                    <Grid key={category.id} size={{lg: 2.3, md: 3, sm: 4, xs: 6}} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 3, cursor: 'pointer'}}>
+                        <Category {...category} />
+                    </Grid>
+                ))}
+            </Grid>
+        </>
     )
 }
