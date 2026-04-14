02bc6172e0415ea15b3cbb642e2ca59ab67a2922
naila-saleh <nailasaleh2004@gmail.com>
2026-04-14 23:48:18 +0300

    enable go to google map from the marker on my map

--- src/components/contactUs/MyLocationMap.jsx
+++ src/components/contactUs/MyLocationMap.jsx
@@ -3,6 +3,13 @@ import {Marker, APIProvider, Map} from "@vis.gl/react-google-maps";
 
 const MyLocationMap = () => {
     const location = {lat: 32.19340, lng: 34.96049};
+
+    const openInGoogleMaps = () => {
+        const {lat, lng} = location;
+        const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+        window.open(url, "_blank", "noopener,noreferrer");
+    };
+
     return <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
         <Box sx={{height: '550px', width: '100%', py: 5, mx: 'auto'}}>
             <Map
@@ -11,7 +18,7 @@ const MyLocationMap = () => {
                 gestureHandling={'greedy'}
                 disableDefaultUI={false}
             >
-                <Marker position={location} />
+                <Marker position={location} onClick={openInGoogleMaps}/>
             </Map>
         </Box>
     </APIProvider>
