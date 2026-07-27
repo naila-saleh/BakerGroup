02bb33b3570a8ebb81ca827b113be030a1018946
naila-saleh <nailasaleh2004@gmail.com>
2026-07-27 12:31:24 +0300

    fix: reset scroll position on route navigation

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import {Container} from "@mui/material";
 import {Outlet} from "react-router-dom";
 import Box from "@mui/material/Box";
+import ScrollToTop from "../components/ScrollToTop.jsx";
 
 export default function AuthLayout() {
     return (
         <Box sx={{backgroundColor: '#2D5356', height: '100vh', px: 2, py: 3}}>
+            <ScrollToTop />
             <Container maxWidth={'sm'} sx={{backgroundColor: 'info.light', borderRadius: 3}}>
                 <Outlet />
             </Container>
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from "../components/navbar/Navbar.jsx";
 import {Outlet} from "react-router-dom";
 import Footer from "../components/footer/Footer.jsx";
 import NavbarResponsiveMenu from "../components/navbar/NavbarResponsiveMenu.jsx";
+import ScrollToTop from "../components/ScrollToTop.jsx";
 
 export default function MainLayout() {
     return (
         <>
+            <ScrollToTop />
             <NavbarResponsiveMenu />
             <Outlet />
             <Footer />
